95796f6853f8aab773e68c3d738dd4a71a13dd34
Sebastian Khoo Yoong-Min <unparallelbehavior@gmail.com>
2017-08-11 02:39:11 +0800

    Player movement tweaks
    Changed
    -aiming walk speed from 250 to 275
    -walking from 500 to 325
    -running from 750 to 650

--- Meeting/Intermediate/Build/Win64/UE4Editor/Development/Meeting/PCH.Meeting.cpp
+++ Meeting/Intermediate/Build/Win64/UE4Editor/Development/Meeting/PCH.Meeting.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/User/Desktop/Unreal/GitHub/FYP_DumbTesting/Meeting/Intermediate/Build/Win64/UE4Editor/Development/Meeting/PCH.Meeting.h"
+#include "C:/Users/Unpar/Documents/FYP_DumbTesting/Meeting/Intermediate/Build/Win64/UE4Editor/Development/Meeting/PCH.Meeting.h"
